212409ae52513dc3d1ecb64bf17736cd7b1d4f8a
Ole Pearse-Danker <ole.pd@icloud.com>
2023-11-25 16:18:54 +0100

    added sidedbar

---
+++ multipage_app/pages/About.py
@@ -0,0 +1,14 @@
+import streamlit as st  
+
+def about_us():
+    st.header("About Us - WATT-Meister")
+    st.write("Welcome to WATT-Meister, your trusted consulting partner for the transition to renewable energy and climate change solutions.")
+    st.write("We are passionate about driving positive change by helping businesses, organizations, and individuals navigate the challenges of energy transition and address climate change.")
+    st.subheader("Our Services")
+    st.write("- Strategy Development\n- Technology Evaluation\n- Risk Assessment\n- Policy Compliance\n- Capacity Building")
+    st.subheader("Why Choose Us")
+    st.write("- Deep expertise and tailored solutions\n- Holistic approach considering economic, environmental, and social aspects\n- Focus on innovation and staying updated\n- Dedicated to client success")
+    st.write("Contact WATT-Meister today to embark on your sustainable energy journey and contribute to a greener future.")
+    st.write("*Inspired by your vision, powered by renewable energy.*")
+
+about_us()
---
+++ multipage_app/pages/Contact.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.header("Contact")
+
+st.write("Please do not contact us in case of problems")
